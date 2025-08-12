44ec60069cd525e0a5c180ffcd1e409feedb7c73
ganeshgfx <ganeshghutiya@live.com>
2025-08-12 22:48:23 +0530

    Modified: vite.config.js,

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
      plugins: [react()],
      base: "/admin/",
+     build: {
+          target: "esnext", // Use modern JS
+          minify: "esbuild", // Faster + smaller than terser
+          cssCodeSplit: true, // Load CSS per chunk
+          chunkSizeWarningLimit: 500, // Avoid large chunk warnings
+          rollupOptions: {
+               output: {
+                    manualChunks(id) {
+                         if (id.includes("node_modules")) {
+                              return id
+                                   .toString()
+                                   .split("node_modules/")[1]
+                                   .split("/")[0]
+                                   .toString();
+                         }
+                    },
+               },
+          },
+     },
 });
